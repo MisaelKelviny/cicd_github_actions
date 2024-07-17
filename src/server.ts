eb9c600e0bb43ec43a891258b8054986744990f6
KelvinyMisael <misaelkelviny@gmail.com>
2024-07-17 01:18:59 -0300

    fix: ajustando messagem

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-import { app } from './app';
-import { env } from './config/env';
+import { app } from "./app";
+import { env } from "./config/env";
 
 app
-  .listen({ port: env.PORT, host: '0.0.0.0' })
-  .then(() => console.log(`🚀 Server is listening on port ${ env.PORT }`));
+  .listen({ port: env.PORT, host: "0.0.0.0" })
+  .then(() =>
+    console.log(`🚀 Server is ready and listening on port ${env.PORT}`)
+  );
